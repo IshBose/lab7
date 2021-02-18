65d57804c1a0064c7d4063858c102db4e193e829
Ishaan Bose <ibose@ucsd.edu>
2021-02-18 10:43:08 -0800

    New files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var projects = require('../projects.json');
 
+
 /*
  * GET home page.
  */
 
 exports.view = function(request, response){
   	response.render('index', projects);
+	data['viewAlt'] = false;
+	response.render('index',data);
+	  
 };
+
+exports.viewAlt = function(request,response){
+	data['viewAlt'] = true;
+	response.render('index',data);
+}
